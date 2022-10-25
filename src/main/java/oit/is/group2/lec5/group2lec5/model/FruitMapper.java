d27472db69c4a54ceb5b545272eff0a3f5cb6ce3
YutoMina <e1b20096@st.oit.ac.jp>
2022-10-25 15:49:59 +0900

    sample5-3

--- src/main/java/oit/is/group2/lec5/group2lec5/model/FruitMapper.java
+++ src/main/java/oit/is/group2/lec5/group2lec5/model/FruitMapper.java
@@ -1,14 +1,22 @@
+
 package oit.is.group2.lec5.group2lec5.model;
 
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Delete;
 
 @Mapper
 public interface FruitMapper {
 
   @Select("SELECT ID, NAME,PRICE FROM FRUIT")
   ArrayList<Fruit> selectAllFruit();
+
+  @Select("select ID, NAME,PRICE from FRUIT WHERE ID = #{id}")
+  Fruit selectById(int id);
+
+  @Delete("DELETE FROM FRUIT WHERE ID =#{id}")
+  boolean deleteById(int id);
 
 }
