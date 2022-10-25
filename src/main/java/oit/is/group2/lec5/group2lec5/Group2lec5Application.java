7a46bfe9ac828fe6c11c4b510e80d4fc32fbb285
e1b20092 <e1b20092@st.oit.ac.jp>
2022-10-25 13:58:47 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group2/lec5/group2lec5/Group2lec5Application.java
@@ -0,0 +1,13 @@
+package oit.is.group2.lec5.group2lec5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2lec5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2lec5Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group2/lec5/group2lec5/Group2lec5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group2.lec5.group2lec5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2lec5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
