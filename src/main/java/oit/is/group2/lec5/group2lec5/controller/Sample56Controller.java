c5a5512398495cd98794364d738b2e0eb72c4ac0
e1b20092 <e1b20092@st.oit.ac.jp>
2022-10-25 16:15:27 +0900

    sample5-6-1

---
+++ src/main/java/oit/is/group2/lec5/group2lec5/controller/Sample56Controller.java
@@ -0,0 +1,49 @@
+package oit.is.group2.lec5.group2lec5.controller;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import oit.is.group2.lec5.group2lec5.service.AsyncCountFruit56;
+
+@Controller
+@RequestMapping("/sample56")
+public class Sample56Controller {
+
+  private final Logger logger = LoggerFactory.getLogger(Sample56Controller.class);
+
+  @Autowired
+  private AsyncCountFruit56 ac56;
+
+  @GetMapping("step1")
+  public SseEmitter pushCount() {
+
+    logger.info("pushCount");
+
+    final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);//
+
+    try {
+      this.ac56.count(emitter);
+    } catch (IOException e) {
+      logger.warn("Exception:" + e.getClass().getName() + ":" + e.getMessage());
+      emitter.complete();
+    }
+    return emitter;
+  }
+
+  // @GetMapping("step2")
+  // public SseEmitter pushFruit() {
+  // // infoレベルでログを出力する
+  // logger.info("pushFruit");
+  // final SseEmitter sseEmitter = new SseEmitter();
+  // this.ac56.pushFruit(sseEmitter);
+  // return sseEmitter;
+  // }
+
+}
